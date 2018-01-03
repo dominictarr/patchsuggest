824288685e0654888a6e5eb691a09863f498e47b
Dominic Tarr <dominic.tarr@gmail.com>
2018-01-03 22:56:13 +1300

    breaking change: have a compose and a search suggestor

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 
 
-exports.gives = { suggest: true }
+exports.gives = { suggest: { compose: true, search: true } }
 
 exports.needs = {
   sbot: { names: { getSignifies: 'first' } }
 }
 
 exports.create = function (api) {
-  function lookup (word, cb) {
-    api.sbot.names.getSignifies(word.substring(1), function (err, names) {
-      if(err) return cb(err)
-      cb(null, names.map(function (e) {
-        return {title: e.name, value: '[@'+e.name+']('+e.id+')'}
-      }))
-    })
+  function create (map) {
+    function lookup (word, cb) {
+      api.sbot.names.getSignifies(word.substring(1), function (err, names) {
+        if(err) return cb(err)
+        cb(null, names.map(map))
+      })
+    }
+    return function (word) {
+      if(word[0] == '@' && word.length > 2) return lookup
+    }
   }
-
   return {
-    suggest: function (word) {
-      if(word[0] === '@' && word.length > 2)
-        return lookup
+    suggest: {
+      compose: create(function (e) {
+        return {title: e.name, id: e.id, value: '[@'+e.name+']('+e.id+')'}
+      }),
+      search: create(function (e) {
+        return {title: e.name, value: e.id}
+      })
     }
   }
 }
+
+
+
+
+
+
 
 
 
